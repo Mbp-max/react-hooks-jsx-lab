31fe7c0ba5a5c8316920e3dc6ce84523d9fde62e
Mario Brito <mariobrito94@hotmail.com>
2022-03-16 17:25:16 -0400

    commit tree 5a7494001789c8063a845bda6f54ad7868fb4ba8

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
+
+
+// const blueColor = {
+//   color: 'blue'
+// } 
+
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const h1Style = {
+    color: 'firebrick',
+  }; 
+  function click(){
+    console.log("im being clicked")
+  };
+  return <div id="home">
+    <h1 style={h1Style} onClick={click}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
